aedbab7a678577cf2d715fc10654a673944ee271
Maykon L Fonseca <maykonlfonseca@gmail.com>
2017-04-11 13:34:47 -0300

    add client storage implementation

---
+++ client.go
@@ -0,0 +1,72 @@
+package mongo
+
+import (
+	"errors"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"gopkg.in/oauth2.v3"
+	"gopkg.in/oauth2.v3/models"
+)
+
+func NewClientStore(config *Config) (cs *MongoClientStore) {
+	cs = &MongoClientStore {config: config}
+
+	session, err := mgo.Dial(cs.config.URL)
+	if err != nil {
+		return
+	}
+	cs.session = session
+
+	err = cs.c(cs.collectionName).EnsureIndex(mgo.Index{
+		Key: []string{"id"},
+		Unique:     true,
+		DropDups:   true,
+		Background: true,
+	})
+	if err != nil {
+		return
+	}
+
+	return cs
+}
+
+// MongoClientStore client information store
+type MongoClientStore struct {
+	config *Config
+	collectionName string
+	session *mgo.Session
+}
+
+// GetByID according to the ID for the client information
+func (cs *MongoClientStore) GetByID(id string) (cli oauth2.ClientInfo, err error) {
+	session := cs.session.Copy()
+	client := new (models.Client)
+	err = session.DB(cs.config.DB).C(cs.collectionName).Find(bson.M{"id": id}).One(client)
+	if err != nil {
+		cli = client
+		return
+	}
+
+	err = errors.New("not found")
+	return
+}
+
+// Set set client information
+func (cs *MongoClientStore) Set(id string, cli oauth2.ClientInfo) (err error) {
+	session := cs.session.Copy()
+	_, err = session.DB(cs.config.DB).C(cs.collectionName).Upsert(bson.M{
+		"id": id,
+	},
+	&models.Client{
+		ID: cli.GetID(),
+		Secret: cli.GetSecret(),
+		Domain: cli.GetDomain(),
+		UserID: cli.GetUserID(),
+	})
+
+	return
+}
+
+func (cs *MongoClientStore) c(name string) *mgo.Collection {
+	return cs.session.DB(cs.config.DB).C(name)
+}
