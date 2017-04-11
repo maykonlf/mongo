4314a31af2c0b525caf51c8ac0915c51152dac56
Maykon L Fonseca <maykonlfonseca@gmail.com>
2017-04-11 14:04:49 -0300

    fix parse client result to Client type

--- client.go
+++ client.go
@@ -40,10 +40,10 @@ type MongoClientStore struct {
 // GetByID according to the ID for the client information
 func (cs *MongoClientStore) GetByID(id string) (cli oauth2.ClientInfo, err error) {
 	session := cs.session.Copy()
-	client := new (models.Client)
-	err = session.DB(cs.config.DB).C(cs.collectionName).Find(bson.M{"id": id}).One(client)
+	var client models.Client
+	err = session.DB(cs.config.DB).C(cs.collectionName).Find(bson.M{"id": id}).One(&client)
 	if err != nil {
-		cli = client
+		cli = &client
 		return
 	}
 
